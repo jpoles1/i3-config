4ae17ca0ac33d9f51cebf055b61e9e55afadba61
¨Jordan <jpoles1@gmail.com>
2016-04-24 18:07:07 -0500

    Added screen config for TV. Setup new monitor for I3.

---
+++ screens.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI1 --mode 1680x1050 --pos 3200x0 --rotate normal --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VGA1 --mode 1280x1024 --pos 1920x0 --rotate normal
