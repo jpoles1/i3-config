2d057e81bdb5f03ccefcf42edc587393421a6b08
¨Jordan <jpoles1@gmail.com>
2016-04-25 11:41:39 -0500

    Many improvements. More colorful!

--- screens.sh
+++ screens.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI1 --mode 1680x1050 --pos 3200x0 --rotate normal --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VGA1 --mode 1280x1024 --pos 1920x0 --rotate normal
+xrandr --output HDMI1 --mode 1680x1050 --pos 1920x0 --rotate normal --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VGA1 --mode 1280x1024 --pos 3600x0 --rotate normal
