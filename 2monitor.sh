c62af16a46afa6ec458b14943040802b474029ce
Jordan Poles <jpoles1@gmail.com>
2017-03-20 13:59:30 -0500

    Updating with the most recent config and monitorblank loadout

---
+++ 2monitor.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1-1 --mode 1920x1080 --pos 2560x224 --rotate normal --output VGA-1-1 --off --output HDMI-1-1 --primary --mode 2560x1080 --pos 0x0 --rotate normal
---
+++ 3monitor.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-1-1 --primary --mode 2560x1080 --pos 1280x0 --rotate normal --output VIRTUAL1 --off --output eDP-1-1 --mode 1920x1080 --pos 3840x224 --rotate normal --output VGA-1-1 --mode 1280x1024 --pos 0x56 --rotate normal
